b6be370adbd358169fc0de8b596bd9a695dfc9e2
emrizkiem <mr.hobskoy@gmail.com>
2019-06-16 16:36:34 +0700

    initial project

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db = 'buku';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Microsoft Azure | Dicoding</title>
+</head>
+<body>
+    
+</body>
+</html>

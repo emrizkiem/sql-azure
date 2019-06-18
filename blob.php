079031a7cb123e8c487c799405c4b219820fba99
emrizkiem <mr.hobskoy@gmail.com>
2019-06-18 19:11:39 +0700

    Fixed Project

--- blob.php
+++ blob.php
@@ -1,32 +1,3 @@
-<?php
-
-require_once 'vendor/autoload.php';
-require_once "./random_string.php";
-
-use MicrosoftAzure\Storage\Blob\BlobRestProxy;
-use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
-use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
-use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
-use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
-
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=nasystorage;AccountKey=SXWwmYuRwoohUvibFtYaOzZ8ivryEqiLWACOnt/ZgptfjkyF3AKeEjKqIP2sXfGOefQoeOBPBrQAo8ZMRV4unw==;";
-
-$containerName = "emrizkiem";
-// Create blob client.
-$blobClient = BlobRestProxy::createBlobService($connectionString);
-if (isset($_POST['submit'])) {
-	$fileToUpload = strtolower($_FILES["fileToUpload"]["name"]);
-	$content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
-	// echo fread($content, filesize($fileToUpload));
-	$blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-	header("Location: blob.php");
-}
-$listBlobsOptions = new ListBlobsOptions();
-$listBlobsOptions->setPrefix("");
-$result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -39,67 +10,81 @@ $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
         body {
             font-family: Source Code Pro, monospace;
         }
-
-        table {
-            border-collapse: collapse;
-        }
-
-        th,
-        td {
-            font-size: 16px;
-            border: 1px solid #DEDEDE;
-            padding: 3px 5px;
-            color: #303030;
-        }
-
-        th {
-            background: #cccccc;
-            font-size: 16px;
-            border-color: #b0b0b0;
-        }
     </style>
 </head>
 
 <body>
-    <h2 style="font-style: bold">Analisa Cover Buku</h2>
-    <p>Pilih foto cover buku yang sesuai.<br> Kemudian klik tombol <b>Upload</b>, untuk menganalisa foto klik tombol <b>Analisis</b> pada tabel.</p><br>
+    <?php
 
-    <form action="blob.php" method="POST" enctype="multipart/form-data">
-        <input type="file" name="fileToUpload" accept=".png,.jpg,.jpeg" style="font-family: Source Code Pro, monospace; font-size: 16px;" required >
-        <input type="submit" name="submit" value="Upload" style="font-family: Source Code Pro, monospace; font-size: 16px;" />
-    </form><br><br>
+    require_once 'vendor/autoload.php';
+    require_once "./random_string.php";
 
-    <table>
-        <thead>
-            <tr>
-                <th>Nama Foto</th>
-                <th>Link File</th>
-                <th>Aksi</th>
-            </tr>    
-        </thead>
-        <tbody>
-            <?php
-                do {
-                    foreach($result->getBlobs() as $blob) {
-                        ?>
-                        <tr>
-                            <td><?php echo $blob->getName() ?></td>
-                            <td><?php echo $blob->getUrl() ?></td>
-                            <td>
-                                <form action="computervision.php" method="post">
-                                    <input type="hidden" name="url" value="<?php echo $blob->getUrl() ?>" />
-                                    <input type="submit" name="submit" value="Analisis" style="font-family: Source Code Pro, monospace; font-size: 16px;" />
-                                </form>
-                            </td>
-                        </tr>
-                        <?php
-                    }
-                    $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-                } while($result->getContinuationToken());
-            ?>
-        </tbody>
-    </table>
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+    use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+    use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+    use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+    use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+
+    $connectionString = "DefaultEndpointsProtocol=https;AccountName=nasystorage;AccountKey=SXWwmYuRwoohUvibFtYaOzZ8ivryEqiLWACOnt/ZgptfjkyF3AKeEjKqIP2sXfGOefQoeOBPBrQAo8ZMRV4unw==;";
+
+    $blobClient = BlobRestProxy::createBlobService($connectionString);
+
+    $fileToUpload = "cover-kotlin.jpg";
+
+    $createContainerOptions = new CreateContainerOptions();
+
+    $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+
+    $createContainerOptions->addMetaData("key1", "value1");
+    $createContainerOptions->addMetaData("key2", "value2");
+
+    $containerName = "emrizkiem" . generateRandomString();
+
+    try {
+        $blobClient->createContainer($containerName, $createContainerOptions);
+
+        $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
+        fclose($myfile);
+
+        echo "Uploading BlockBlob: " . PHP_EOL;
+        echo $fileToUpload;
+        echo "<br/>";
+
+        $content = fopen($fileToUpload , "r");
+
+        $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+
+        $listBlobsOptions = new ListBlobsOptions();
+        $listBlobsOptions->setPrefix("Buku");
+
+        echo "These are the blobs present in the container: ";
+
+        do {
+            $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+            foreach ($result->getBlobs() as $blob) {
+
+                echo $blob->getName() . ": " . $blob->getUrl ( ) . "<br />";
+            }
+
+            $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+        } while ($result->getContinuationToken());
+           echo "<br/>";
+
+         echo "This is the content of the blob uploaded: ";
+        $blob = $blobClient->getBlob($containerName, $fileToUpload);
+        fpassthru($blob->getContentStream() ) ;
+          echo "<br />";
+    } catch (ServiceException $e) {
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code . ": " .  $error_message . "<br />";
+    } catch (InvalidArgumentTypeException $e) {
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code . ": " .  $error_message . "<br />";
+    }
+    ?>
+
 </body>
+
 </html>
--- computervision.php
+++ computervision.php
@@ -1,113 +1,84 @@
-<?php
-    if(isset($_POST['submit'])) {
-        if(isset($_POST['url'])) {
-            $url = $_POST['url'];
-        } else {
-            header("Location: blob.php");
-        }
-    } else {
-        header("Location: blob.php");
-    }
-?>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Microsoft Azure | Dicoding</title>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
     <style>
         body {
             font-family: Source Code Pro, monospace;
-        }
-
-        table {
-            border-collapse: collapse;
-        }
-
-        th,
-        td {
-            font-size: 16px;
-            border: 1px solid #DEDEDE;
-            padding: 3px 5px;
-            color: #303030;
-        }
-
-        th {
-            background: #cccccc;
-            font-size: 16px;
-            border-color: #b0b0b0;
         }
     </style>
 </head>
 
 <body>
-    <h2 style="font-style: bold">Hasil Analisa Cover Buku</h2>
-    <p>Berikut ini merupakan hasil analisa foto cover buku yang anda upload.<br> Karakteristik foto tersebut terdapat di kolom <b>Response</b>.</p>
-    
     <script type="text/javascript">
-        $(document).ready(function() {
-            // Replace <Subscription Key> with your valid subscription key.
+        function processImage() {
             var subscriptionKey = "62ead22b8b044cf2951612a22ffbd366";
-
-            // This region.
-            var uriBase = "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
-
-            // Request parameters.
+            var uriBase =
+                "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
             var params = {
                 "visualFeatures": "Categories,Description,Color",
                 "details": "",
                 "language": "en",
             };
 
-            // Display the image.
-            var sourceImageUrl = "<?php echo $url ?>";
+            var sourceImageUrl = document.getElementById("inputImage").value;
             document.querySelector("#sourceImage").src = sourceImageUrl;
 
-            // Make the REST API call.
             $.ajax({
-                url: uriBase + "?" + $.param(params),
- 
-                // Request headers.
-                beforeSend: function(xhrObj){
-                    xhrObj.setRequestHeader("Content-Type","application/json");
-                    xhrObj.setRequestHeader(
-                        "Ocp-Apim-Subscription-Key", subscriptionKey);
-                },
- 
-                type: "POST",
- 
-                // Request body.
-                data: '{"url": ' + '"' + sourceImageUrl + '"}',
-            })
+                    url: uriBase + "?" + $.param(params),
 
-            .done(function(data) {
-                // Show formatted JSON on webpage.
-                $("#responseTextArea").val(JSON.stringify(data, null, 2));
-            }) 
+                    beforeSend: function(xhrObj) {
+                        xhrObj.setRequestHeader("Content-Type", "application/json");
+                        xhrObj.setRequestHeader(
+                            "Ocp-Apim-Subscription-Key", subscriptionKey);
+                    },
 
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                // Display error message.
-                var errorString = (errorThrown === "") ? "Error. " :
-                    errorThrown + " (" + jqXHR.status + "): ";
-                errorString += (jqXHR.responseText === "") ? "" :
-                    jQuery.parseJSON(jqXHR.responseText).message;
-                alert(errorString);
-            });
-        });
+                    type: "POST",
+
+                    data: '{"url": ' + '"' + sourceImageUrl + '"}',
+                })
+
+                .done(function(data) {
+                    $("#responseTextArea").val(JSON.stringify(data, null, 2));
+                    $("#desc").text(data.description.captions[0].text);
+
+                })
+
+                .fail(function(jqXHR, textStatus, errorThrown) {
+                    var errorString = (errorThrown === "") ? "Error. " :
+                        errorThrown + " (" + jqXHR.status + "): ";
+                    errorString += (jqXHR.responseText === "") ? "" :
+                        jQuery.parseJSON(jqXHR.responseText).message;
+                    alert(errorString);
+                });
+        }
     </script>
-
-    <div id="wrapper" style="width:1020px; display:table;">
-        <div id="jsonOutput" style="width:600px; display:table-cell;">
+    <h1>Analyze image:</h1>
+    Enter the URL to an image, then click the <strong>Analyze image</strong> button.
+    <br><br>
+    Image to analyze:
+    <input type="text" name="inputImage" id="inputImage" value="" />
+    <button onclick="processImage();" style="font-family: Source Code Pro, monospace;">Analyze image</button>
+    <br><br>
+    <div id=" wrapper" style="width:1020px; display:table;">
+        <div id="jsonOutput" style="width:600px; display:table-cell;" style="font-family: Source Code Pro, monospace;">
             Response:
             <br><br>
-            <textarea id="responseTextArea" class="UIInput" style="width:580px; height:400px;"></textarea>
+            <textarea id=" responseTextArea" class="UIInput" style="width:580px; height:400px;"></textarea>
         </div>
-        <div id="imageDiv" style="width:420px; display:table-cell;">
+        <div id="imageDiv" style="width:420px; display:table-cell;" style="font-family: Source Code Pro, monospace;">
             Source image:
             <br><br>
-            <img id="sourceImage" width="400" />
-        </div>
+            <img id=" sourceImage" width="400" />
+        <br>
+        <p id="desc"></p>
+    </div>
     </div>
 </body>
+
 </html>
